9f2a1a96049af4738ca66df79602c40ee903f457
CodingShambles <victortheveloper@gmail.com>
2023-10-18 13:04:07 -0300

    adding outer functions to cat pkg

--- cat.go
+++ cat.go
@@ -1,9 +1,21 @@
 package cat
 
+import (
+	grown "github.com/VictorHCBR/adult_cat"
+)
+
 func Meow() string {
 	return "Meow"
 }
 
 func Meows() string {
 	return "Meow Meow Meow"
+}
+
+func GrownMeow() string {
+	return grown.GrownCat(Meow())
+}
+
+func GrownMeows() string {
+	return grown.GrownCat(Meows())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/VictorHCBR/cat
 
 go 1.21.3
+
+require github.com/VictorHCBR/adult_cat v0.0.0-20231018155448-4811351441ad
